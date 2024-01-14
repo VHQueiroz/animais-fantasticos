67f292a7801e5598af071c5efb6f77dc04e05419
Victor Hugo Moreira Queiroz <vitorhugomq8@hotmail.com>
2024-01-14 15:11:51 -0300

    refatorando com eslint

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,19 +1,15 @@
-export default function initAccordion(){
+export default function initAccordion() {
   const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
   const activeClass = 'ativo';
-  if(accordionList.length){
-    accordionList[0].classList.add(activeClass)
-    accordionList[0].nextElementSibling.classList.add(activeClass)
-    
-    
-    function activeAccordion(){
-      this.nextElementSibling.classList.toggle(activeClass);
-      this.classList.toggle(activeClass);
-    }
-    
+  function activeAccordion() {
+    this.nextElementSibling.classList.toggle(activeClass);
+    this.classList.toggle(activeClass);
+  }
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
     accordionList.forEach((item) => {
       item.addEventListener('click', activeAccordion);
     });
   }
 }
-  
--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,33 +1,28 @@
-export default function initAnimaNumeros(){
-
-  function animaNumeros(){
-    const numeros = document.querySelectorAll('[data-numero]')
-  numeros.forEach( numero => {
-    const total = +numero.innerText;
-    const incremento = Math.floor(total /100);
-    let start = 0;
-    const timer = setInterval(() => {
-    start = start + incremento;
-    numero.innerText = start;
-    if(start > total){
-      clearInterval(timer);
-      numero.innerText = total;
+export default function initAnimaNumeros() {
+  function animaNumeros() {
+    const numeros = document.querySelectorAll('[data-numero]');
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      const incremento = Math.floor(total / 100);
+      let start = 0;
+      const timer = setInterval(() => {
+        start += incremento;
+        numero.innerText = start;
+        if (start > total) {
+          clearInterval(timer);
+          numero.innerText = total;
+        }
+      }, 25 * Math.random());
+    });
+  }
+  let observer;
+  function handleMutation(mutation) {
+    if (mutation[0].target.classList.contains('ativo')) {
+      observer.disconnect();
+      animaNumeros();
     }
-    }, 25 * Math.random())
-  })
-  }
-  
-  function handleMutation(mutation){
-  if(mutation[0].target.classList.contains('ativo')){
-    observer.disconnect();
-    animaNumeros();
-  }
-  
   }
   const observeTarget = document.querySelector('.numeros');
-  const observer = new MutationObserver(handleMutation);
-  
-  observer.observe(observeTarget, {attributes: true});
+  observer = new MutationObserver(handleMutation);
+  observer.observe(observeTarget, { attributes: true });
 }
-
-
